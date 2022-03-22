7ba509bbd8fb079aa65ffafaf0802e66139a546a
maharani14 <99779863+maharani14@users.noreply.github.com>
2022-03-22 19:11:05 +0800

    barang.java

---
+++ barang.java
@@ -0,0 +1,15 @@
+package javakasir;
+public class barang {
+int id;
+String namaBarang;
+int stockBarang, hargaBarang, terjual;
+
+    public barang(int id, String namaBarang, int stockBarang, int hargaBarang, int terjual) {
+        this.id = id;
+        this.namaBarang = namaBarang;
+        this.stockBarang = stockBarang;
+        this.hargaBarang = hargaBarang;
+        this.terjual = terjual;
+    }
+
+}
